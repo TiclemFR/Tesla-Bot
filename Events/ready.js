70994e95c8079c4ddd1af3528493ff5184cf9229
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2019-07-11 17:21:16 +0200

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -2,7 +2,7 @@ module.exports = async(client) => {
 
     client.user.setPresence({
         game: {
-            name: "t!help --> Afficher l\'aide | Nouvelle fonctionnalité: Musique |"
+            name: "t!help --> Afficher l\'aide | Dernier ajout: Musique |"
         }
     })
 };
