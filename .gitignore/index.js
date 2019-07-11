01d93aa7d6d9d0e2cb19a1ce286622ae1c43c2ab
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2019-07-11 16:31:01 +0200

    Add files via upload

---
+++ .gitignore/index.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const fs = require('fs');
+
+client.login('NTkwMjEyNDAxNjUyMTcwODQz.XQjCSw.GeXaZJLJunr80LEj0Hm7tmPItQg');
+
+client.commands = new Discord.Collection();
+
+
+fs.readdir('./Commandes/', (error, f) => {
+    if(error) console.error(error);
+
+    let commandes = f.filter(f => f.split('.').pop() === 'js');
+    if(commandes.length <= 0) return console.log('Aucune commande trouvée !');
+
+    commandes.forEach((f) =>{
+        let commande = require(`./Commandes/${f}`);
+        console.log(`${f} commande chargée !`);
+
+        client.commands.set(commande.help.name, commande);
+    });
+});
+
+fs.readdir('./Events/', (error, f) => {
+    if(error) console.error(error);
+    console.log(`${f.length} events en chargement`);
+
+    f.forEach((f) => {
+        const events = require(`./Events/${f}`);
+        const event = f.split('.')[0];
+
+        client.on(event, events.bind(null, client));
+    });
+});
