e67d2a70d01284dadc95bb201d3e858e13a762ba
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2019-07-11 17:11:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 const fs = require('fs');
 
-client.login('NTkwMjEyNDAxNjUyMTcwODQz.XQjCSw.GeXaZJLJunr80LEj0Hm7tmPItQg');
+client.login(process.env.token);
 
 client.commands = new Discord.Collection();
 
