2d328163593d98893eaa71869d40b78cfde6a20f
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2019-07-15 22:47:49 +0200

    Update errors.js

--- modules/errors.js
+++ modules/errors.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const fs = require("fs");
-let config = require("botconfig.json");
+let config = require("../modules/botconfig.json");
 
 module.exports.noPerms = (message, perm) => {
     let embed = new Discord.RichEmbed()
