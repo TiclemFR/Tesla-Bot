dbfc0d3f04ff0b1ee65559069c052d52dd1ee685
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2019-07-16 20:27:50 +0200

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -2,7 +2,7 @@ module.exports = async(client) => {
 
     client.user.setPresence({
         game: {
-            name: "t!help --> Afficher l\'aide | Dernier ajout: Kick | Ban"
+            name: "t!help --> Afficher l\'aide | Dernier ajout: Serveur info"
         }
     })
 };
