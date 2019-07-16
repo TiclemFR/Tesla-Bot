0dda409a418d40ae9665b16e0bd949611a8ac675
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2019-07-16 17:04:59 +0200

    Create serverinfo.js

---
+++ Commandes/serverinfo.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const moment = require('moment')
+
+module.exports.run = async (bot, message, args) => {
+    let sicon = message.guild.iconURL;
+    let serverembed = new Discord.RichEmbed()
+    .setDescription("Server Information")
+    .setColor("#15f153")
+    .setThumbnail(sicon)
+    .addField("Nom du server:", message.guild.name)
+    .addField("Crée le:", moment.utc(message.guild.createdAt).format('LLL'))
+    .addField("Vous avez rejoins le:", moment.utc(message.member.joinedAt).format('LLL'))
+    .addField("Membres total:", message.guild.memberCount);
+
+    message.channel.send(serverembed);
+}
+
+module.exports.help = {
+  name:"serverinfo"
+}
