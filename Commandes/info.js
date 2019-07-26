5a150a49a461bf157a0f97f800b5929f32979838
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2019-07-26 22:42:34 +0200

    Update info.js

--- Commandes/info.js
+++ Commandes/info.js
@@ -23,7 +23,7 @@ module.exports.run = async(client, message, args) => {
                 },
                 {
                     name: '⚙ __Version du Bot__',
-                    value: '1.6.3'
+                    value: '1.6.5'
                 },
                 {
                     name: '🏳__Langue du Bot__',
