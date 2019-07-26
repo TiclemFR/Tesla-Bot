99c02dc48fd2140c1693ffb418217fca5f6035bc
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2019-07-26 22:41:55 +0200

    Update serverinfo.js

--- Commandes/serverinfo.js
+++ Commandes/serverinfo.js
@@ -4,10 +4,10 @@ const moment = require('moment')
 module.exports.run = async (bot, message, args) => {
     let sicon = message.guild.iconURL;
     let serverembed = new Discord.RichEmbed()
-    .setDescription("Server Information")
+    .setDescription("Informations du Serveur")
     .setColor("#15f153")
     .setThumbnail(sicon)
-    .addField("Nom du server:", message.guild.name)
+    .addField("Nom du serveur:", message.guild.name)
     .addField("Crée le:", moment.utc(message.guild.createdAt).format('LLL'))
     .addField("Vous avez rejoins le:", moment.utc(message.member.joinedAt).format('LLL'))
     .addField("Membres total:", message.guild.memberCount);
