5b52f2c0755166fbbdc5b467ce622d65b13e3f34
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2019-08-09 18:02:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const money = require("../modules/money.json")
+const money = require("modules/money.json")
 const fs = require('fs');
 
 client.login('NTkwMjEyNDAxNjUyMTcwODQz.XQjCSw.GeXaZJLJunr80LEj0Hm7tmPItQg');
