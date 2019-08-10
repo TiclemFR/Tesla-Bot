330c0729b174daa55c3d7a207faee0ad9e375387
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2019-08-10 14:49:19 +0200

    Add files via upload

---
+++ Commandes/money.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const money = require('../modules/money.json');
+
+module.exports.run = async (bot, message, args) => {
+    if(!money[message.author.id]) {
+        money[message.author.id] = {
+            money: 0
+        }
+    }
+
+    let userMoney = money[message.author.id].money;
+
+    let userMoneyEmbed = new Discord.RichEmbed()
+    .setAuthor(message.author.username)
+    .setColor('#f36c06')
+    .addField('Votre argent:', `💰 ${userMoney}`)
+
+    message.channel.send(userMoneyEmbed);
+};
+
+module.exports.help = {
+    name: "money"
+};
