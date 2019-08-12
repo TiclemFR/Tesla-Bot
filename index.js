183343e8e2a3e99500a66b05b347467a7729c17d
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2019-08-12 21:05:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ fs.readdir('./Events/', (error, f) => {
         client.on(event, events.bind(null, client));
     });
     //SYSTEME MONEY
-module.exports = async(client, message, args) => {
+module.exports = {
     if(!money[message.author.id]) {
         money[message.author.id] = {
             money: 0
