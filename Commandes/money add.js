9e82f023fb1dee95292652132c1cf4e9cd4bb2f4
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2020-02-26 14:39:47 +0100

    Create money add.js

---
+++ Commandes/money add.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const money = require('../modules/money.json');
+
+module.exports.run = async (bot, message, args) => {
+    if(!money[message.author.id]) {
+        money[message.author.id] = {
+            money: 0
+        }
+    }
+
+    let userMoney = money[message.author.id].money;
+    userMoney = userMoney + 10;
+
+    let userMoneyEmbed = new Discord.RichEmbed()
+    .setAuthor(message.author.username)
+    .setColor('#f36c06')
+    .addField('Votre argent est maintenant de:', `💰 ${userMoney}`)
+
+    message.channel.send(userMoneyEmbed);
+};
+
+module.exports.help = {
+    name: "money add"
+};
