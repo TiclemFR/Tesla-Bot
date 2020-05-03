0ad7ab71083258739948e7eab52f7bde7c44779c
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2020-05-03 20:30:42 +0200

    Update ban.js

--- Commandes/ban.js
+++ Commandes/ban.js
@@ -14,12 +14,12 @@ module.exports.run = async (bot, message, args) => {
     if(bUser.id === bot.user.id) return errors.botuser(message); 
     let bReason = args.join(" ").slice(22);
     if(!bReason) return errors.noReason(message.channel);
-    if(bUser.hasPermission("MANAGE_MESSAGES")) return errors.equalPerms(message, bUser, "MANAGE_MESSAGES");
+    if(bUser.hasPermission("BAN_MEMBERS")) return errors.equalPerms(message, bUser, "BAN_MEMBERS");
 
     let banEmbed = new Discord.RichEmbed()
     .setDescription("~Ban~")
     .setColor("#bc0000")
-    .addField("Banned User", `${bUser} with ID ${bUser.id}`)
+    .addField("Banned User", `<@${bUser}> with ID ${bUser.id}`)
     .addField("Banned By", `<@${message.author.id}> with ID ${message.author.id}`)
     .addField("Banned In", message.channel)
     .addField("Time", moment.utc(message.createdAt).format('LLL'))
