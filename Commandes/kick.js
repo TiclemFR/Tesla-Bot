b0d01a60e7b0af52257ee9c64fa343caf15c6569
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2020-05-03 20:35:26 +0200

    Update kick.js

--- Commandes/kick.js
+++ Commandes/kick.js
@@ -12,12 +12,12 @@ module.exports.run = async (bot, message, args) => {
     let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!kUser) return errors.cantfindUser(message.channel);
     let kReason = args.join(" ").slice(22);
-    if(kUser.hasPermission("MANAGE_MESSAGES")) return errors.equalPerms(message, kUser, "MANAGE_MESSAGES");
+    if(kUser.hasPermission("KICK_MEMBERS")) return errors.equalPerms(message, kUser, "KICK_MEMBERS");
 
     let kickEmbed = new Discord.RichEmbed()
     .setDescription("~Kick~")
     .setColor("#e56b00")
-    .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
+    .addField("Kicked User", `<@${kUser}> with ID ${kUser.id}`)
     .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
     .addField("Kicked In", message.channel)
     .addField("Time", moment.utc(message.createdAt).format('LLL'))
