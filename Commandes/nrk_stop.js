ebb28128d8637d49fcfbb5077b30334bb3cf1e6b
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2020-05-11 17:26:28 +0200

    Create nrk_stop.js

---
+++ Commandes/nrk_stop.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+
+module.exports.run = async(bot, message, args) => {
+
+    const voiceChannel = message.member.voiceChannel;
+    voiceChannel.leave()
+   
+
+module.exports.help = {
+    name: 'nrk_stop'
+};
