3b2136d4a565849ad005ada638424882de34d442
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2020-05-11 17:34:57 +0200

    Update nrk_play.js

--- Commandes/nrk_play.js
+++ Commandes/nrk_play.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 
-module.exports.run = async(bot, message, args) => {
+module.exports.run = async(client, message, args) => {
 
     if (!message.member.voiceChannel) return message.channel.send('❌ Connectez vous à un salon vocal !')
     if (message.guild.me.voiceChannel) return message.channel.send('❌ Le Bot est déjà connecté à un salon !')
@@ -8,7 +8,7 @@ module.exports.run = async(bot, message, args) => {
     
     const voiceChannel = message.member.voiceChannel;
     voiceChannel.join()
-    const broadcast = bot.voice.createBroadcast();
+    const broadcast = client.voice.createBroadcast();
     broadcast.play('https://lyd.nrk.no/nrk_radio_mp3_mp3_h');
     message.channel.send(`NRK mp3 en cours de l'ecture !`);
 };
