cb5c9247c35daaf97b0f796aa1249f7b82e7c6a9
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2020-05-11 17:42:41 +0200

    Update nrk_play.js

--- Commandes/nrk_play.js
+++ Commandes/nrk_play.js
@@ -9,8 +9,7 @@ module.exports.run = async(bot, message, args) => {
     
     const voiceChannel = message.member.voiceChannel;
     voiceChannel.join()
-    const broadcast = client.voice.createBroadcast();
-    broadcast.play('https://lyd.nrk.no/nrk_radio_mp3_mp3_h');
+    const dispatcher = connection.play('https://lyd.nrk.no/nrk_radio_mp3_mp3_h');
     message.channel.send(`NRK mp3 en cours de l'ecture !`);
 };
 
