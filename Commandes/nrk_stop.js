affd3a3c36d55d08637f83876838e45dc3ae49c0
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2020-05-11 17:29:24 +0200

    Update nrk_stop.js

--- Commandes/nrk_stop.js
+++ Commandes/nrk_stop.js
@@ -2,9 +2,12 @@ const Discord = require('discord.js');
 
 module.exports.run = async(bot, message, args) => {
 
-    const voiceChannel = message.member.voiceChannel;
-    voiceChannel.leave()
-   
+    if (!message.member.voiceChannel) return message.channel.send('❌ Connectez vous à un salon vocal !');
+    if (!message.guild.me.voiceChannel) return message.channel.send('❌ Le Bot n\'est pas connecté !');
+
+    if (message.guild.me.voiceChannelID !== message.member.voiceChannelID) return message.channel.send('❌ Vous n\'êtes pas dans le même salon !');
+    message.guild.me.voiceChannel.leave();
+};
 
 module.exports.help = {
     name: 'nrk_stop'
