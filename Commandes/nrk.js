e96c50ae42ce4b902c8a2dd038894525cf3dd058
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2020-05-11 17:05:26 +0200

    Create nrk.js

---
+++ Commandes/nrk.js
@@ -0,0 +1,16 @@
+var Discord = require('discord.js')
+
+module.exports.run = async(bot, message, args) => {
+
+    if (!message.member.voiceChannel) return message.channel.send('❌ Connectez vous à un salon vocal !')
+    if (message.guild.me.voiceChannel) return message.channel.send('❌ Le Bot est déjà connecté à un salon !')
+
+    
+    const voiceChannel = message.member.voiceChannel;
+    const dispatcher = connection.playFile('https://lyd.nrk.no/nrk_radio_mp3_mp3_h');
+    message.channel.send(`NRK mp3 en cours de l'ecture !`);
+};
+
+module.exports.help = {
+    name: 'nrk_play'
+};
