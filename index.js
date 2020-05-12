0e0c6e9b99c0d091a58a24a3990c215a760334cd
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2020-05-12 16:45:31 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const money = require("./modules/money.json")
+const fs = require('fs');
+
+client.login(process.env.token);
+
+client.commands = new Discord.Collection();
+
+
+fs.readdir('./Commandes/', (error, f) => {
+    if(error) console.error(error);
+
+    let commandes = f.filter(f => f.split('.').pop() === 'js');
+    if(commandes.length <= 0) return console.log('Aucune commande trouvée !');
+
+    commandes.forEach((f) =>{
+        let commande = require(`./Commandes/${f}`);
+        console.log(`${f} commande chargée !`);
+
+        client.commands.set(commande.help.name, commande);
+    });
+});
+
+fs.readdir('./Events/', (error, f) => {
+    if(error) console.error(error);
+    console.log(`${f.length} events en chargement`);
+
+    f.forEach((f) => {
+        const events = require(`./Events/${f}`);
+        const event = f.split('.')[0];
+
+        client.on(event, events.bind(null, client));
+    });
+
+});
