07b76a9de8fa056d6ef9491ce690e6a35c9d894f
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2020-05-12 16:51:41 +0200

    .

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const money = require("./modules/money.json")
 const fs = require('fs');
 
 client.login(process.env.token);
