d22ac4f4b99c20c11d76570a3915b88ef259d4bf
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2020-05-13 21:31:19 +0200

    role

--- Commandes/addrole.js
+++ Commandes/addrole.js
@@ -7,14 +7,13 @@ module.exports.run = async(client, message, args) => {
     if (!message.guild.member(client.user.id).hasPermission('MANAGE_ROLES')) { return message.channel.send('Je n\'ai pas la permission `gérer les roles` !'); }
     
     let member = message.mentions.members.first() || message.guild.members.find(m => m.user.tag === args[0]) || message.guild.members.get(args[0]);
-    let role = message.guild.roles.cache.find(role => role.name === args[1]); //message.guild.roles.find(r => r.name == args[1]) || message.guild.roles.find(r => r.id == args[1]) || message.mentions.roles.first();
-
+    let role = message.guild.roles.cache.find(role => role.name === args[1]);
     if(!member){return message.channel.send('Vous devez mentionner un utilisateur !');}
     if (!role) { return message.channel.send('Ce role n\'existe pas !'); }
-    if (member.roles.cache.has(role.id)) { return message.channel.send('Vous avez déjà ce role !'); }
+    if (member.roles.cache.has(role.id)) { return message.channel.send('Ce membre a déjà ce role !'); }
     
         member.roles.add(role.id)
-            .then(() => message.channel.send(member +' à désormais le role ' + role.toString()))
+            .then(() => message.channel.send(member.tag +' à désormais le role ' + role.toString()))
             .catch(console.error);
 };
 
--- Commandes/delrole.js
+++ Commandes/delrole.js
@@ -6,14 +6,14 @@ module.exports.run = (client, message, args) => {
     if (!message.guild.member(client.user.id).hasPermission('MANAGE_ROLES')) { return message.channel.send('Je n\'ai pas la permission `gérer les roles` !'); }
 
      let member = message.mentions.members.first() || message.guild.members.find(m => m.user.tag === args[0]) || message.guild.members.get(args[0]);
-    let role = message.guild.roles.find(r => r.name == args[1]) || message.guild.roles.find(r => r.id == args[1]) || message.mentions.roles.first();
+    let role = message.guild.roles.cache.find(role => role.name === args[1]);
 
     if(!member){return message.channel.send('Vous devez mentionner un utilisateur !');}
     if (!role) { return message.channel.send('Ce role n\'existe pas !'); }
-    if (!member.roles.has(role.id)) { return message.channel.send('Vous n\'avez pas ce role !'); }
+    if (!member.roles.cache.has(role.id)) { return message.channel.send('Ce membre n\'a pas ce role !'); }
     
-        member.removeRole(role.id)
-            .then(() => message.channel.send(member +' n\'à désormais plus le role ' + role.toString()))
+        member.roles.remove(role.id)
+            .then(() => message.channel.send(member.tag +' n\'à désormais plus le role ' + role.toString()))
             .catch(console.error);
 };
 
