23887a85f849dc5dc641748b9746be5ce7d8fc1b
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2020-05-13 16:40:00 +0200

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async(client) => {
 
-    client#setActivity({
+    client.setActivity({
         game: {
             name: "t!help --> Afficher l\'aide | Dernier ajout: Liste des problèmes"
         }
