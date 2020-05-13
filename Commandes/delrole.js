cf15ed992953e5031037cafba0e94864d93d06fe
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2020-05-13 21:39:22 +0200

    Update delrole.js

--- Commandes/delrole.js
+++ Commandes/delrole.js
@@ -5,7 +5,7 @@ module.exports.run = (client, message, args) => {
     if (!message.guild.member(message.author.id).hasPermission('MANAGE_ROLES')) { return message.channel.send('Vous n\'avez pas la permission `gérer les roles` !'); }
     if (!message.guild.member(client.user.id).hasPermission('MANAGE_ROLES')) { return message.channel.send('Je n\'ai pas la permission `gérer les roles` !'); }
 
-     let member = message.mentions.members.first() || message.guild.members.find(m => m.user.tag === args[0]) || message.guild.members.get(args[0]);
+    let member = message.mentions.members.first();// || message.guild.members.find(m => m.user.tag === args[0]) || message.guild.members.get(args[0]);
     let role = message.guild.roles.cache.find(role => role.name === args[1]);
 
     if(!member){return message.channel.send('Vous devez mentionner un utilisateur !');}
