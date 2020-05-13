3ac1ada68e3b67015e386c051ec9bf9e75b01da9
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2020-05-13 15:25:11 +0200

    Create info.js

---
+++ Commandes/info.js
@@ -0,0 +1,59 @@
+const Discord = require('discord.js');
+const moment = require('moment');
+
+module.exports.run = async(client, message, args) => {
+
+    const membre = message.mentions.members.first();
+
+    message.channel.send({
+        embed: {
+            color: 0xc006eb,
+            title: 'Information du bot',
+            thumbnail: {
+                url: "https://cdn.discordapp.com/attachments/589885773231423507/706574129951932436/logo2.jpg"
+            },
+            fields: [
+                {
+                    name: '🔰 __Créateur__',
+                    value: 'Ticlem'
+                },
+                {
+                    name: '📍 __Pseudo__',
+                    value: 'Tesla BOT'
+                },
+                {
+                    name: '⚙ __Version du Bot__',
+                    value: '⚠ **ALPHA** 0.2'
+                },
+                {
+                    name: '🏳__Langues Disponibles__',
+                    value: '🇫🇷FR'
+                },
+                {
+                    name: '✏ __Préfix__',
+                    value: 't!'
+                },
+                {
+                    name: '🗒️ __Émojis Utilisés__',
+                    value: '❌: Erreur/Action impossible | ✅: Action réalisé | ⚠: Problème détecté/Avertissement'
+                },
+                {
+                    name: '🌐 __Website__',
+                    value: 'https://thetesla.voxan24.me/'
+                },
+                {
+                    name: '🔄 __Developpement__',
+                    value: 'Prochaine fonctionnalité de __**Tesla BOT**__: **__A venir__**'
+                },
+            ],
+            footer: {
+                text: `Information de Tesla Bot`
+            }
+        }
+    })
+};
+
+
+module.exports.help = {
+    name: "info"
+}
