797205e26c5071f165c80b555b6dc9cb31ce00bb
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2020-05-13 16:45:28 +0200

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async(client) => {
 
-    client.presences({
+    client.presences.set({
         game: {
             name: "t!help --> Afficher l\'aide | Dernier ajout: Liste des problèmes"
         }
