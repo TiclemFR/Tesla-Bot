fb49519ed44888aab23386ba4dc2e45618459dd0
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2020-05-13 14:07:34 +0200

    Update help.js

--- Commandes/help.js
+++ Commandes/help.js
@@ -5,7 +5,7 @@ module.exports.run = async(client, message, args) => {
 
     const membre = message.mentions.members.first();
     
-    const EmbedHelp = new Discord.MessageEmbed();
+    const EmbedHelp = new Discord.MessageEmbed()
     .setColor('#136ee2')
     .setTitle('Liste des commandes disponible')
     .addFields('⚙️ __**Général**__', 'Commandes principales')
