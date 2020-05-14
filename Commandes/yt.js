3febaab4e06c0fd984e6ab15473ef994e83af35f
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2020-05-14 20:08:21 +0200

    Update yt.js

--- Commandes/yt.js
+++ Commandes/yt.js
@@ -8,10 +8,14 @@ module.exports.run = async(client, message, args) => {
 	const voiceChannel = message.member.voice.channel;
 	if(!voiceChannel){return message.reply('❌ Connectez-vous d\'abord dans un salon vocal')};
 	if(!args[0]){return message.reply('❌ Indiquez un lien Youtube')};
+	const validate = await ytdl.validateURL(args[0]);
+	if(!validate){return message.reply('❌ L\'URL n\'est pas valide')};
 
+	const info = await ytdl.getInfo(args[0]);
 	voiceChannel.join().then(connection => {
 	const stream = ytdl(args[0], { filter: 'audioonly' });
 	const dispatcher = connection.play(stream);
+	message.channel.send(`Musique ajoutée: ${info.title}`);
 
 	dispatcher.on('end', () => voiceChannel.leave());
 
