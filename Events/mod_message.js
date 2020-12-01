d92e86291437aa2167f68920cc1338ed5b30e07f
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2020-12-01 20:25:52 +0100

    Delete mod_message.js

--- Events/mod_message.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js');
-const TAB = 30;
-
-    client.on('message', message => {
-        if (message.content === 'ça va') {
-            message.channel.send('ok');
-            }
-        })
