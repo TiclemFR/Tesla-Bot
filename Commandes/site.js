5a613a3b6eb7d24199bbd09f4c4c811a9935e560
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2020-12-01 19:31:43 +0100

    update

--- Commandes/site.js
+++ Commandes/site.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports.run = async(client, message, args) => {
-    await message.channel.send('....').then(async(m) => await m.edit('https://thetesla.voxan24.me/'));
+    await message.channel.send('....').then(async(m) => await m.edit('http://thetesla.my-heberg.fr/'));
 };
 
 module.exports.help = {
