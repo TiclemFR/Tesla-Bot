8730d7087ed47b56e875369d4e0fc26ee6c0503a
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2020-12-01 20:24:47 +0100

    Update mod_message.js

--- Events/mod_message.js
+++ Events/mod_message.js
@@ -1,10 +1,8 @@
 const Discord = require('discord.js');
 const TAB = 30;
 
-module.exports = async(client, message) => {
     client.on('message', message => {
         if (message.content === 'ça va') {
             message.channel.send('ok');
             }
         })
-};
