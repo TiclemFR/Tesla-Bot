0053f1c15ce3b86abb38c01f3db6a93febedba8d
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2020-12-02 10:02:07 +0100

    update

--- Commandes/neko.js
+++ Commandes/neko.js
@@ -3,7 +3,7 @@ const { get } = require('https');
 module.exports.run = async(client, message, args) => {
 
     if(!message.channel.nsfw) {
-        return message.channel.send('❌ ERREUR, 🔞 cette commande contient du contenu pouvant heurter la sensibilité des plus jeunes, il doit être utilisé dans un salon NSFW !');
+        return message.channel.send('❌ ERREUR, 🔞 cette commande contient du contenu pouvant heurter la sensibilité des plus jeunes, elle doit être utilisé dans un salon NSFW !');
     }
 
     get('https://neko-love.xyz/api/v1/neko', (res) => {
