793b7b073cbfee14903f98a43f04ac6b9d1efd2c
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2021-05-03 20:06:17 +0200

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -1,4 +1,11 @@
 module.exports = async(client) => {
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
 
-    client.user.setActivity("Aide: t!help || Traverse le sytème solaire 🚀", { type: "PLAYING"});
+    setInterval(function(){
+        var tab = ['Aide: t!help || Traverse le sytème solaire 🚀', 'Aide: t!help || Cherche Baro 🕵️‍♂️','🕵️‍Aide: t!help || Admire le néant 😲']
+        client.user.setActivity(tab[getRandomInt(3)], { type: "PLAYING"});
+    })
+    
 };
