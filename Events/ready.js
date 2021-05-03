f80932848129cc8d92fbf63a92b19b4fdbe0a3db
TiclemFR <52786228+TiclemFR@users.noreply.github.com>
2021-05-03 20:07:08 +0200

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -6,6 +6,6 @@ module.exports = async(client) => {
     setInterval(function(){
         var tab = ['Aide: t!help || Traverse le sytème solaire 🚀', 'Aide: t!help || Cherche Baro 🕵️‍♂️','🕵️‍Aide: t!help || Admire le néant 😲']
         client.user.setActivity(tab[getRandomInt(3)], { type: "PLAYING"});
-    })
+    }, 5000)
     
 };
